d7cc3d2895eefd6b07cce71f17e4fd6de478cc7b
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-04-28 12:44:05 +0000

    trying to store pokemon data in an array

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,48 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css'
 
 function App() {
 
   const api = ` https://pokeapi.co/api/v2/`
-  
-  useEffect(() => {
-    fetch(api) 
-      .then(response => response.json())
-      .then(data => console.log(data))
-  }, [])
+  const [pokemonArray, setPokemonArray] = useState([]);
+    
+  const getData = async () => {
+    try {
+      const response = await fetch(api);
+      const json = await response.json();
+      const pokemon = json.pokemon;
+      const pokemonResponse = await fetch(pokemon);
+      const pokemonJson = await pokemonResponse.json();
+      const results = pokemonJson.results;
+
+      for (let i = 0; i < 10; ++i) {
+        const pokemon = results[i]
+        const pokemonUrl = pokemon.url;
+        const responsePokemonUrl = await fetch(pokemonUrl);
+        const pokemonJson = await responsePokemonUrl.json();
+        const pokemonName = pokemonJson.name;
+        const pokemonImageUrl = pokemonJson.sprites.front_default;
+        const newPokemonObject = {
+          id: crypto.randomUUID,
+          name: pokemonName,
+          url: pokemonImageUrl
+        }
+        await setPokemonArray((prevPokemonArray) => ([...prevPokemonArray, newPokemonObject]));
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  getData();
+  console.log(pokemonArray);
   return (
     <>
+      {pokemonArray.map((pokemon) => {
+        <div className='pokemon'>
+          <img src={pokemon.url} alt="" />
+          <p className="name">{pokemon.name}</p>
+        </div>
+      })}
     </>
   )
 }
