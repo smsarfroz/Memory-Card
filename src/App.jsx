5783b640329dfe6b3d0fccc95cdd5305f35c5007
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-04-30 04:21:13 +0000

    styling the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import './App.css'
-import { transformWithEsbuild } from 'vite';
 
 function App() {
 
@@ -17,22 +16,7 @@ function App() {
         const pokemonJson = await pokemonResponse.json();
         const results = pokemonJson.results;
 
-        // for (let i = 0; i < 10; ++i) {
-        //   const pokemon = results[i];
-        //   const pokemonUrl = pokemon.url;
-        //   const responsePokemonUrl = await fetch(pokemonUrl);
-        //   const pokemonJson = await responsePokemonUrl.json();
-        //   const pokemonName = pokemonJson.name;
-        //   const pokemonImageUrl = pokemonJson.sprites.front_default;
-        //   const newPokemonObject = {
-        //     id: crypto.randomUUID(),
-        //     name: pokemonName,
-        //     url: pokemonImageUrl
-        //   }
-        //   setPokemonArray((prevPokemonArray) => [...prevPokemonArray, newPokemonObject]);
-        // }
-
-        const pokemons = await Promise.all(results.slice(0, 10).map(async (pokemon) => {
+        const pokemons = await Promise.all(results.slice(0, 12).map(async (pokemon) => {
           const pokemonUrl = pokemon.url;
           const responsePokemonUrl = await fetch(pokemonUrl);
           const pokemonJson = await responsePokemonUrl.json();
@@ -58,16 +42,28 @@ function App() {
     }
   }, []);
   return (
-    <>
-      {pokemonArray.map((pokemon) => {
-        return (  
-          <div className='pokemon' key={pokemon.id}>
-            <img src={pokemon.url} alt="" />
-            <p className="name">{pokemon.name}</p>
-          </div>
-        );
-      })}
-    </>
+    <div className='page'>
+      <div className="header">
+        <div className="left">
+          <h1>Pokemon Memory Card Game</h1>
+          <p>Get points by clicking on pokemons, but don't click on the same pokemon twice!</p>
+        </div>
+        <div className="right">
+          <p>Score: </p>
+          <p>Best Score: </p>
+        </div>
+      </div>
+      <div className='container'>
+        {pokemonArray.map((pokemon) => {
+          return (  
+            <div className='pokemon' key={pokemon.id}>
+              <img src={pokemon.url} alt="" />
+              <p className="name">{pokemon.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   )
 }
 
