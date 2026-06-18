45b80daeeabd7cf5668840a62d63a72be1dbb7f7
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2026-06-18 04:04:53 +0000

    docs: add screenshots to readme

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -10,16 +10,16 @@ describe('something truthy and falsy', () => {
   });
 });
 
-// import { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
-// import App from './App';
+import App from './App';
 
-// describe('App', () => {
-//   it('renders headline', () => {
-//     render(<App title="React" />);
+describe('App', () => {
+  it('renders headline', () => {
+    render(<App title="React" />);
 
-//     screen.debug();
+    screen.debug();
 
-//     // check if App components renders headline
-//   });
-// });
+    // check if App components renders headline
+  });
+});
